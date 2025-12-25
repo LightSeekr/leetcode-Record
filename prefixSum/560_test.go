adaf545cfdf62101738bbaf178b20efa04b47734
Harold Peng <3522373084@qq.com>
2025-12-25 17:14:19 +0800

    前缀和

---
+++ prefixSum/560_test.go
@@ -0,0 +1,31 @@
+package prefixSum
+
+import "testing"
+
+func Test_subarraySum(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				nums: []int{1, 2, 1, 2, 1},
+				k:    3,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subarraySum(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("subarraySum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
